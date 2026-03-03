0531ff826d14aacdcd2e9533a996b22b2fc4beaa
Anassbouderhem <as.bouderhem@gmail.com>
2026-03-03 18:32:36 +0100

    feat: add product form with validation and save functionality

---
+++ src/main/java/org/example/gestiondesproduits/sec/SecurityConfig.java
@@ -0,0 +1,43 @@
+package org.example.gestiondesproduits.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+    //La strategy in memory authentification
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        PasswordEncoder encoder = passwordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(encoder.encode("123")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("123")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("123")).roles("USER","ADMIN").build()
+                );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**","/delete/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/public/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .build();
+
+
+}
+}
--- src/main/java/org/example/gestiondesproduits/web/ProductController.java
+++ src/main/java/org/example/gestiondesproduits/web/ProductController.java
@@ -19,7 +19,7 @@ import java.util.LongSummaryStatistics;
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList",products);
@@ -27,22 +27,22 @@ public class ProductController {
     }
     @GetMapping("/")
     public String home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
-    @GetMapping("/newProduct")
+    @GetMapping("/admin/newProduct")
     public String newProduct(Model model){
         model.addAttribute("product", new Product());
         return "new-product";
     }
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
-    @PostMapping("/saveProduct")
+    @PostMapping("/admin/saveProduct")
     public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model){
         if (bindingResult.hasErrors()) return "new-product";
         productRepository.save(product);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 }
